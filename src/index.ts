0f566667fd0b8cee626520cd1f191093246d8f8e
Bryanschauerte <bryan.schauerte@gmail.com>
2021-11-01 17:01:04 -0500

    WIP, able to get git diff

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello ts')
+import { getDiffFromHead } from './gitDifference';
+
+const showDiff = async () => {
+  try {
+    const x = await getDiffFromHead();
+    console.log(x?.split(/\n/), 'x'); // trim out non docs directories
+  } catch (e) {
+    console.log('ERRR', e);
+  }
+};
+
+showDiff();
