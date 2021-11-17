2707908c533c13f8ecb76dee45ca3ed9f384f717
Bryanschauerte <bryan.schauerte@gmail.com>
2021-11-17 16:31:30 -0600

    APP-5750, wip upload and handing of images

--- packages/community-docs/src/utils/constants.ts
+++ packages/community-docs/src/utils/constants.ts
@@ -1,8 +1,9 @@
 // temporary until get env file set up
-import {authToken} from '../auth';
-export const FLAG_FOR_DELETE:string ='FILE_DOES_NOT_EXIST'
-export const SUPPORTED_FILE_TYPE_EXTENTIONS:string[]=['.md']
-export const PATH_OF_DIRECTORY_TO_WATCH:string = 'docs/';
-export const DEV_URL:string = 'https://jupiterone.vanillastaging.com/api/v2';
-export const Authorization:string=authToken
-export const POST_DELAY_TIME:number=1200
+import { authToken } from "../auth";
+export const FLAG_FOR_DELETE: string = "FILE_DOES_NOT_EXIST";
+export const SUPPORTED_FILE_TYPE_EXTENTIONS: string[] = [".md"];
+export const PATH_OF_DIRECTORY_TO_WATCH: string = "docs/";
+export const DEV_URL: string = "https://jupiterone.vanillastaging.com/api/v2";
+export const Authorization: string = authToken;
+export const POST_DELAY_TIME: number = 1200;
+export const SUPPORTED_MEDIA_TYPES = ["png", "jpg", "jpeg", "gif"];
