f84c46645ca5d9e1ca2b3961bd05df69889ee647
Bryanschauerte <bryan.schauerte@gmail.com>
2021-11-30 17:43:28 -0600

    WIP...

---
+++ packages/community-docs/src/runDiff.ts
@@ -0,0 +1,5 @@
+import { updateCommunityDocsWithPathOverride } from "./";
+import { PATH_OF_DIRECTORY_TO_WATCH } from "./utils/constants";
+export default updateCommunityDocsWithPathOverride([
+  `${PATH_OF_DIRECTORY_TO_WATCH}/compliance-reporting/compliance-import.md`,
+]);
--- packages/community-docs/src/utils/common.ts
+++ packages/community-docs/src/utils/common.ts
@@ -1,16 +1,22 @@
-import {
-    VanillaArticle,
-    VanillaKnowledgeCategory
-} from './types'
+import { VanillaArticle, VanillaKnowledgeCategory } from "./types";
 
 export const isKnowledgeCategoryType = (
-    procedure: VanillaArticle | VanillaKnowledgeCategory
-  ): procedure is VanillaKnowledgeCategory => {
-    return Boolean((procedure as VanillaKnowledgeCategory).procedureType === 'Category');
-  };
-  
-  export const isArticleType = (
-    procedure: VanillaArticle | VanillaKnowledgeCategory
-  ): procedure is VanillaArticle => {
-    return Boolean((procedure as VanillaArticle).procedureType === 'Article');
-  };
+  procedure: VanillaArticle | VanillaKnowledgeCategory
+): procedure is VanillaKnowledgeCategory => {
+  return Boolean(
+    (procedure as VanillaKnowledgeCategory).procedureType === "Category"
+  );
+};
+
+export const isArticleType = (
+  procedure: VanillaArticle | VanillaKnowledgeCategory
+): procedure is VanillaArticle => {
+  return Boolean((procedure as VanillaArticle).procedureType === "Article");
+};
+
+export const createDisplayName = (name: string) => {
+  return name
+    .split(/-|_/g)
+    .map((item) => `${item[0].toUpperCase()}${item.substring(1)}`)
+    .join(" ");
+};
