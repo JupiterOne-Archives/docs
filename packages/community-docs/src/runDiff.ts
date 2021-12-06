5d33555df6d0127809579175925ebf5c912b4984
Bryanschauerte <bryan.schauerte@gmail.com>
2021-12-06 11:52:08 -0600

    debug, env check

--- packages/community-docs/src/runDiff.ts
+++ packages/community-docs/src/runDiff.ts
@@ -5,7 +5,7 @@ const getDiffAndReportIssues = async () => {
     console.log("Running updateCommunityDocs");
     const completed = await updateCommunityDocs();
     console.log(`Bearer ${process.env.TOKEN}`, "request tk");
-    console.log("here?", process.env.VANILLIA_STAGING_ENV_TOKEN);
+    console.log("ENV?", process.env);
     if (completed !== undefined) {
       console.log("Completed", completed);
     } else {
