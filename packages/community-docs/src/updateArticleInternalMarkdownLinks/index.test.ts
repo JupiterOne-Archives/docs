1ce9943b718216d8b90eeff4428d1693a14330c3
Bryanschauerte <bryan.schauerte@gmail.com>
2021-12-11 17:07:33 -0600

    change of replacement

--- packages/community-docs/src/updateArticleInternalMarkdownLinks/index.test.ts
+++ packages/community-docs/src/updateArticleInternalMarkdownLinks/index.test.ts
@@ -5,16 +5,16 @@ import { updateArticleInternalMarkdownLinks } from "./";
 const expectedReturn =
   "\n" +
   "    [1]: https://raw.githubusercontent.com/feathericons/feather/master/icons/settings.svg?sanitize=true\n" +
-  "    https://jupiterone.vanillastaging.com/kb/categories/304-api-key-access\n" +
+  "    https://jupiterone.vanillastaging.com/kb/articles/537-api-key-access\n" +
   "    - If you are an **Enterprise** customer and use SAML SSO, see the instructions\n" +
-  "    [here](https://jupiterone.vanillastaging.com/kb/categories/304-faq-integrations)\n" +
-  "   https://jupiterone.vanillastaging.com/kb/categories/305-alert-rule,\n" +
-  "   https://jupiterone.vanillastaging.com/kb/categories/304-integrations-roadmap,\n" +
-  "   https://jupiterone.vanillastaging.com/kb/categories/304-configure-sso-integration,\n" +
+  "    [here](https://jupiterone.vanillastaging.com/kb/articles/542-faqs-integrations)\n" +
+  "   https://jupiterone.vanillastaging.com/kb/articles/536-alert-rule,\n" +
+  "   https://jupiterone.vanillastaging.com/kb/articles/543-integrations-roadmap,\n" +
+  "   https://jupiterone.vanillastaging.com/kb/articles/540-configure-sso-integration,\n" +
   "    - If you are an **Enterprise** customer and use SAML SSO, see the instructions\n" +
-  "    click [here](https://jupiterone.vanillastaging.com/kb/categories/304-configure-sso-integration)\n" +
+  "    click [here](https://jupiterone.vanillastaging.com/kb/articles/540-configure-sso-integration)\n" +
   " \n" +
-  "   https://jupiterone.vanillastaging.com/kb/categories/304-faqs-aws\n" +
+  "   https://jupiterone.vanillastaging.com/kb/articles/541-faqs-aws\n" +
   "    ";
 describe("updateArtilceInternalMarkdownLinks", () => {
   describe("index", () => {
