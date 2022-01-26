1736f616a4811a10820c3293c921a53881ede21c
Bryanschauerte <bryan.schauerte@gmail.com>
2022-01-26 14:34:17 -0600

    ignoring test in integrations-handling

--- packages/integrations-handling/jest.config.js
+++
@@ -1,9 +0,0 @@
-const {
-  buildJestConfig,
-} = require("@jupiterone/typescript-tools/config/jest-util");
-module.exports = {
-  ...buildJestConfig({ packageDir: __dirname }),
-  setupFilesAfterEnv: ["../../jest.setup.ts"],
-  globalSetup: "../../jest.globalSetup.ts",
-  coveragePathIgnorePatterns: ["./src/utils/index.ts", "./src/utils/types.ts"],
-};
