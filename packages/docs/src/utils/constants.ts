c26d6e84f57ae0354e4b0e996f610d67e755ba4e
Drew Johnson <drew.johnson@jupiterone.com>
2022-05-28 19:19:14 -0500

    No ticket testing (#384)
    * Getting staging auth token
    
    * Getting staging auth token
    
    * Getting staging auth token

--- packages/docs/src/utils/constants.ts
+++ packages/docs/src/utils/constants.ts
@@ -7,8 +7,10 @@ export const DEV_URL: string = "https://jupiterone.vanillastaging.com/api/v2";
 export const PROD_URL: string =
   "https://jupiterone.vanillacommunities.com/api/v2";
 export const Authorization: string = authToken;
-console.log(`STAGING TOKEN ${process.env.TOKEN}`);
-console.log(`STAGING ${JSON.stringify(process.env.TOKEN)}`);
+console.log(`STAGING TOKEN`);
+for (const char of String(process.env.TOKEN)) {
+  console.log(`${char}`);
+}
 export const REQUEST_DELAY: number = 1500;
 export const SUPPORTED_MEDIA_TYPES = ["png", "jpg", "jpeg", "gif"];
 export const MARKDOWN_IMAGE_REGEX = /\]\(((..\/){1,}assets.*?)\)/;
